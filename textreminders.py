9fec06a06785bb777d0cb15a67b87c52e95cde9b
shannyang <shannony@andrew.cmu.edu>
2014-05-15 14:29:04 -0400

    Victory ~
    Email sending works now!

--- textreminders.py
+++ textreminders.py
@@ -1,24 +1,42 @@
+# textreminders.py
+# Script to text email reminders to 
+
 # Modules
 import smtplib
+import time
+import shelve
 from email.mime.text import MIMEText
 
-# Globals
-TEXTFILE = "text.txt" # dummy file for MIMEText
-SERVER = "localhost"
+# Server stuff
+# Security what security
+SERVER = "smtp.gmail.com"
+PORT = 587
+USER = "keystrokeforensics@gmail.com"
+PASS = "youwillneverguessthis" # egregious
 
-def newMessage(subject, message):
-    global TEXTFILE
-    global SERVER
+# Recipient data
+RECIPIENTS = "recipients"
 
-    fp = open(TEXTFILE,"rb")
-    msg = MIMEText(fp.read())
-    s = smtplib.SMTP(SERVER)
-    fp.close()
-    
+def sendMessage(subject,message):
+    global RECIPIENTS
+    recipients = shelve.open(RECIPIENTS)
+    for recipient in recipients:
+        newMessage(subject,message,recipients[recipient])
+    recipients.close()
+            
+def newMessage(subject, message, recipient):
+    global SERVER, PORT, USER, PASS
+
+    msg = MIMEText(message)
+    s = smtplib.SMTP(SERVER,PORT)
+    s.ehlo()
+    s.starttls()
+    s.ehlo()
+    s.login(USER,PASS)
+
     msg["Subject"] = subject
-    msg["From"] = "keystrokeforensics@gmail.com"
-    msg["To"] = "3015237912@tmomail.net"
-
-    s.sendmail(msg["From"], [msg["To"]], message)
+    msg["From"] = USER
+    msg["To"] = recipient
+    
+    s.sendmail(USER, [msg["To"]], msg.as_string())
     s.quit()
-newMessage("hi","maya is a good girl")
